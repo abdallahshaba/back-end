fe5df32822b1b8d6a4ad87e0b67a652cd98dbe07
abdallahshaban <abdallahshaban200@gmail.com>
2024-06-23 17:41:04 +0300

    add getData function to signin screen

--- auth/signin.php
+++ auth/signin.php
@@ -9,19 +9,21 @@ $password = filterRequest("password");
 
 
 
-$stmt = $con->prepare("SELECT * FROM users WHERE `email` = ? AND `password` = ? ");
-$stmt->execute(array($email, $password));
-$count = $stmt->rowCount();
-if ($count > 0) {
-    printSuccess(
-        "Email is Exist"
-    );
-} else {
+// $stmt = $con->prepare("SELECT * FROM users WHERE `email` = ? AND `password` = ? ");
+// $stmt->execute(array($email, $password));
+// $count = $stmt->rowCount();
+// if ($count > 0) {
+//     printSuccess(
+//         "Email is Exist"
+//     );
+// } else {
 
-   printFailure(
-    "Email Is Not Found "
-   );
+//    printFailure(
+//     "Email Is Not Found "
+//    );
 
-}
+// }
+
+getData("users" , "`email` = ? AND `password` = ?" , array($email , $password));
 
 ?>
